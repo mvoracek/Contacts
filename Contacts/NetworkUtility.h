1122bcb5be6368e7f04e6526f33c5c64424e4cdb
mjvoracek <mjvoracek@gmail.com>
2014-11-19 09:55:14 -0600

    first commit

---
+++ Contacts/NetworkUtility.h
@@ -0,0 +1,13 @@
+//
+//  NetworkUtility.h
+//  Contacts
+//
+//  Created by Matthew Voracek on 11/19/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkUtility : NSObject
+
+@end
---
+++ Contacts/NetworkUtility.m
@@ -0,0 +1,13 @@
+//
+//  NetworkUtility.m
+//  Contacts
+//
+//  Created by Matthew Voracek on 11/19/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import "NetworkUtility.h"
+
+@implementation NetworkUtility
+
+@end
