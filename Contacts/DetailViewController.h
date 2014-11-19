deb077d29ea58f7b502ed3fcfca64745a96b50a0
mjvoracek <mjvoracek@gmail.com>
2014-11-19 09:18:33 -0600

    Initial Commit

---
+++ Contacts/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Contacts
+//
+//  Created by Matthew Voracek on 11/19/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Contacts/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Contacts
+//
+//  Created by Matthew Voracek on 11/19/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
